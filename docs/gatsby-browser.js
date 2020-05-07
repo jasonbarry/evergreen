f2abb8439240a2c206534f0c0d1bdef6f75c028b
jasonbarry <jasbarry@me.com>
2020-05-07 11:52:42 -0700

    Changing string

--- docs/gatsby-browser.js
+++ docs/gatsby-browser.js
@@ -3,7 +3,7 @@ const { NODE_ENV = 'development' } = process.env
 export const onRouteUpdate = () => {
   if (!window.analytics || typeof window.analytics.page !== 'function') {
     if (NODE_ENV === 'development') {
-      console.warn('Unable to locate analytics.js')
+      console.warn('Unable to find analytics.js')
     }
 
     return
